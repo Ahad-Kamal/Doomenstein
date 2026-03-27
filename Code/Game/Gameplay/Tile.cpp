aaa08b1198ceb4f67d34b8108daa5ae9e94a4c73
Ahad-Kamal <akamal64@att.net>
2026-03-26 18:07:14 -0600

    Implemented Tile class
    [git-p4: depot-paths = "//depot/C35/Students/ahadk/SD/Doomenstein/": change = 454960]

---
+++ Code/Game/Gameplay/Tile.cpp
@@ -0,0 +1,25 @@
+#include "Game/Gameplay/Tile.hpp"
+#include "Game/Gameplay/TileDefinition.hpp"
+#include "Engine/Math/AABB3.hpp"
+
+
+//-----------------------------------------------------------------------------------------------
+Tile::Tile( IntVec3 coordinates, TileDefinitions const* tileDef )
+	: m_coords( coordinates )
+	, m_tileDef( tileDef )
+{
+}
+
+//-----------------------------------------------------------------------------------------------
+AABB3 Tile::CreateAABB3FromCoords()
+{
+	return AABB3( static_cast<float>( m_coords.x ), static_cast<float>( m_coords.y ), static_cast<float>( m_coords.z ),
+		static_cast<float>( m_coords.x + 1 ), static_cast<float>( m_coords.y + 1 ), static_cast<float>( m_coords.z + 1 ) );
+}
+
+//-----------------------------------------------------------------------------------------------
+bool Tile::IsTileSolid()
+{
+	return m_tileDef->IsSolid();
+}
+
---
+++ Code/Game/Gameplay/Tile.hpp
@@ -0,0 +1,23 @@
+#pragma once
+#include "Engine/Math/IntVec3.hpp"
+#include <vector>
+
+
+//-----------------------------------------------------------------------------------------------
+struct AABB3;
+class TileDefinitions;
+
+//-----------------------------------------------------------------------------------------------
+struct Tile
+{
+public:
+	IntVec3 m_coords;
+	TileDefinitions const* m_tileDef;
+
+public:
+	Tile( IntVec3 coordinates, TileDefinitions const* tileDef );
+	~Tile() = default;
+
+	AABB3 CreateAABB3FromCoords();
+	bool IsTileSolid();
+};
